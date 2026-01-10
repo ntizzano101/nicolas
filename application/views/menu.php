5f5f550f07bd6f013e984237668e139aa310f1d1
ntizzano <ntizzano>
2026-01-10 20:03:51 -0300

    correcciones

--- application/views/menu.php
+++ application/views/menu.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?=base_url()?>">ZAPATA HS</a>
+      <a class="navbar-brand" href="<?=base_url()?>">NICOLAS TIZZANO</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
